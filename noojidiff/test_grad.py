8e44a1d6051793c535578cc7247c2b6b94251ded
anoojpatel <1996anoojpatel@gmail.com>
2024-04-21 22:24:08 -0400

    add 3 var AD to test example for debugging

--- noojidiff/test_grad.py
+++ noojidiff/test_grad.py
@@ -1,17 +1,19 @@
 import numpy as np
 from autodiff import *
 
-a_global, b_global = np.random.rand(4), np.random.rand(4)
+a_global, b_global, c_global = np.random.rand(4), np.random.rand(4), np.random.rand(4)
 
 with tape_context(add_new_tape=True) as tape:
-    def simple(a, b):
+    def simple(a, b, c):
         t = a + b
+        t = a * c
         return t @ b
 
 
     a = Variable.constant(a_global, name='a')
     b = Variable.constant(b_global, name='b')
-    loss = simple(a, b)
+    c = Variable.constant(c_global, name='c')
+    loss = simple(a, b, c)
     da, db = grad(loss, [a, b], tape)
     print("da", da)
     print("db", db)
