get MLP backprop working with correct dimensions and proper reverse mode forumation

noojidiff/autodiff.py
@@ -159,8 +159,8 @@ def operator_matmul(self : Variable, rhs: Variable, gradient_tape: Tape) -> Vari
     def propagate(dL_doutputs: list[Variable]):
         dL_dC, = dL_doutputs
         print(dL_dC, dL_dC.value.ndim == 0)
-        dC_dself = rhs # partial derivative of r = self*rhs
-        dC_drhs = self # partial derivative of r = self*rhs
+        dC_dself = rhs # partial derivative of C = self*rhs
+        dC_drhs = self # partial derivative of C = self*rhs
 
         # Check if primal output is a scalar / zero dimmed scalar overload
         if  C.value.ndim == 0 or np.isscalar(C.value):

noojidiff/modeling/loss.py
@@ -3,7 +3,7 @@ import numpy as np
 def nll_loss(input, target):
     return -input[target]
 
-def nll_loss_backward(dL_dL, Z_curr, target):
+def nll_loss_backward(dL_dL, L_curr, Z_curr, target):
     """ Negative Log-likelihood Backward Pass
 
     We can formulate the backwards pass as the chain rule'd derivative of (-1) * input.getindex(target)
@@ -12,7 +12,10 @@ def nll_loss_backward(dL_dL, Z_curr, target):
             dL_dL: The Cotangent Output
             Z_curr: primal input
             target: primal input
+        Returns:
+            Cotangents inputs to the logits
     """
-    dL_dZ = np.ones_like(Z_curr)
-    dL_dZ[target] = -1 * dL_dL
+    dL_dZ = np.zeros_like(Z_curr)
+    print(dL_dZ.shape)
+    dL_dZ[target] = -1 * dL_dL * L_curr
     return dL_dZ

noojidiff/modeling/mlp.py
@@ -1,4 +1,5 @@
 import numpy as np
+import warnings
 
 def create_MLP(input_size: int, depth: int, width: int, class_n: int):
     """Create input layer, hidden layers, and final layer, using normal(,√1/n) where `n` is the input size of the layer
@@ -9,30 +10,41 @@ def create_MLP(input_size: int, depth: int, width: int, class_n: int):
             width: int number of neurons per layer
             class_n: int the number of output classes            
     """
+    if depth == 1:
+        raise ValueError("Cant have a MLP with depth only 1! You probably mean depth=2, where there is only 1 learnable W matrix")
+    elif depth == 2:
+        warnings.warn("By specifying depth 2, you will not have a hidden layer! `width` is ignored!")
+        return [[np.sqrt(1/input_size) * np.random.normal(size=(class_n, input_size))]]
     return [np.sqrt(1/input_size) * np.random.normal(size=(width, input_size))] + \
-        [np.sqrt(1/width) * np.random.normal(size=(width,width)) for _ in range(depth)] + \
+        [np.sqrt(1/width) * np.random.normal(size=(width,width)) for _ in range(depth-2)] + \
         [np.sqrt(1/width) * np.random.normal(size=(class_n, width))]
 
 def layer(W: np.ndarray,x: np.array):
-    Z = W @ x
+    Z = W.dot(x)
     A = relu(Z)
-    return A
+
+    dC_dself = x 
+    dC_drhs = W 
+    #dL_dself = lambda dL_dC: dL_dC @ dC_dself.T
+    #dL_drhs = lambda dL_dC: dC_drhs.T @ dL_dC
+    return A, Z, #dL_dself, dL_drhs
 
 def relu(Z):
     return np.maximum(0,Z)
 
-def relu_backward(dA, Z):
-    dZ = np.array(dA, copy = True)
-    dZ[Z <= 0] = 0;
-    return dZ;
+def relu_backward(Z):
+    return (Z > 0).astype(Z.dtype)
 
 def layer_backward(dA_curr, W_curr, Z_curr, A_prev):
-    m = A_prev.shape[1]
+    #print(A_prev.shape)
+    #m = A_prev.shape[1]
     
-    backward_activation_func = relu_backward
-    
-    dZ_curr = backward_activation_func(dA_curr, Z_curr)
-    dW_curr = (dZ_curr @ A_prev.T) / m
-    dA_prev = (W_curr.T @ dZ_curr)
+    print(f"dA_curr: {dA_curr.shape}")
+    dZ_curr = dA_curr * relu_backward(Z_curr)
+    print("dW_curr:", dZ_curr.shape, " @ ", A_prev.T.shape)
+    dL_dW_curr = np.dot(dZ_curr, A_prev.T) 
+    dL_dA_prev = np.dot(W_curr.T, dZ_curr)
+    print("dA_prev:", W_curr.T.shape, " @ ", dZ_curr.shape)
+    print(f"dA_prev res: {dL_dA_prev.shape}, dw_curr res:{dL_dW_curr.shape}")
 
-    return dA_prev, dW_curr
+    return dL_dW_curr, dL_dA_prev

noojidiff/modeling/network.py
@@ -1,13 +1,63 @@
 import numpy as np
-from noojidiff.modeling.loss import nll_loss
-from noojidiff.modeling.mlp import create_MLP, layer, relu, relu_backward, layer_backward
+from noojidiff.modeling.loss import nll_loss, nll_loss_backward
+from noojidiff.modeling.mlp import create_MLP, layer, layer_backward
 
-mlp = create_MLP(4, 2, 5, 3)
-x = np.random.rand(4)
-activation = x
-activations = []
-#print(mlp)
-for W in mlp:
-    activations.append(activation:= layer(W,activation))
+mlp = create_MLP(4, 4, 5, 3) # can clean up later to make depth calculation be cleaner, but here 2 is hidden depth
+x = np.random.rand(4,1) # single 4 dim'd input
+y = np.array([2]) # Class label 2 for our example
 
-print([activation.shape for activation in activations])
+
+def forward(mlp, x):
+    activation = x 
+    activations = {}
+    activations_counter = 0
+    activations[activations_counter] = activation
+    activations_counter +=1
+    logits = {}
+    counter = 0
+    for W in mlp:
+        print(f"x_in: {activation}")
+        activation, logit = layer(W, activation)
+        activations.update({activations_counter: activation}), logits.update({counter:logit})
+        print( f" A_{counter}:{activation}, W:{W.shape}")
+        counter += 1
+        activations_counter += 1
+    return activations, logits
+    
+activations, logits = forward(mlp, x)
+
+print("mlp depth:",len(mlp))
+y_hat = nll_loss(activations[3], y)
+activations.update({"y_hat": y_hat})
+#print("logits: ",logits)
+print(f"y_hat: {y_hat}")
+
+
+# Compute backwards passes
+dL_dL = 1.0 # We seed the backwards pass, because of the chain-rule, ∂L/∂L = 1
+
+
+def full_mlp_backpropagate(mlp, activations, logits, y):
+    dL_dL = 1.0
+    # compute derivative of activations w.r.t. nll_loss
+    dA = nll_loss_backward(dL_dL, activations["y_hat"], activations[len(mlp)], y)
+    cotangents = [dA]
+    print(f"==== y_hat: {dA.shape}=======")
+    weight_grads = {}
+    for i in reversed(range(len(mlp))):
+        l, curr_logits, curr_activations = mlp[i], logits[i], activations[i]
+        assert dA.shape == curr_logits.shape, f"{dA.shape} != {curr_logits.shape}"
+        dA_W, dA = layer_backward(dA, l, curr_logits, curr_activations)
+        cotangents.append(dA)
+        if i in weight_grads:
+            weight_grads[i] += dA_W
+        else:
+            weight_grads[i] = dA_W
+
+    return weight_grads, cotangents
+        
+
+weight_grads,cots = full_mlp_backpropagate(mlp, activations, logits, y)
+print("weights:", [weight_grads[i].shape for i in reversed(range(4))])
+print("cotangents: ",[cot.shape for cot in cots])
+print("activations: ", [activations[i].T.shape for i in reversed(range(4))])