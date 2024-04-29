9e83f734f396e868dc5929f2aa2f9e780619d789
anoojpatel <1996anoojpatel@gmail.com>
2024-04-29 12:04:13 -0400

    changes from the live walkthrough

--- noojidiff/modeling/loss.py
+++ noojidiff/modeling/loss.py
@@ -19,3 +19,28 @@ def nll_loss_backward(dL_dL, L_curr, Z_curr, target):
     print(dL_dZ.shape)
     dL_dZ[target] = -1 * dL_dL #* L_curr
     return dL_dZ
+
+
+#def softmax(input, i):
+#   return np.exp(input[i]) / np.sum( np.exp(input)  )
+
+def apply_softmax(input):
+    return np.exp(input) / np.sum( np.exp(input)  )
+
+def softmax_backward(dL_dNLL, P_curr, Z_curr):
+    dL_dNLL
+    dL_dZ = np.zeros_like(dL_dNLL)
+    n = len(dL_dNLL)
+    Df = np.zeros((n,n))
+    for i in range(len(dL_dZ)):
+        for j in range(len(dL_dZ)):
+            if i != j:
+                Df[j,i] =  -P_curr[i]*P_curr[j]
+            else:
+                Df[j,i] = -P_curr[i] + P_curr[i]**2
+
+    for j in range(n):
+        dL_dZ[j] = np.dot(Df[j],dL_dNLL)
+
+    return dL_dZ
+
